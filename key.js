a381a0465bb06e60a52228bb2949d843e082e177
John Brady <John Brady>
2017-04-08 01:22:53 -0400

    Pushing initial files

---
+++ key.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'tUjcoEWi3LMh1UmILrOWeXu00',
+  consumer_secret: 'Lsq9wKXFLdkjMGsxZb6GJenFA07jHjvs6VT99iGWtDzafhxheH',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
